d14487497fb6ec9812cc50f22175a2a6fe12818b
igor <igorek888rane@gmail.com>
2023-03-21 19:16:36 +0300

    favicon

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import styles from './CatalogPage.module.scss'
 
 const CatalogPage: FC = () => {
-	return <main className={styles.catalog}></main>
+	return <main className={styles.catalog}>Catalog</main>
 }
 
 export default CatalogPage
