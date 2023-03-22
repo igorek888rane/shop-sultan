0b40095a03d5922cf462524607373cfa68a3c903
igor <igorek888rane@gmail.com>
2023-03-22 20:23:06 +0300

    products

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,8 +1,13 @@
 import { FC } from 'react'
 import styles from './CatalogPage.module.scss'
+import products from '../../data/data.json'
 
 const CatalogPage: FC = () => {
-	return <main className={styles.catalog}></main>
+	return (
+		<main className={styles.catalog}>
+			<div>Catalog</div>
+		</main>
+	)
 }
 
 export default CatalogPage
