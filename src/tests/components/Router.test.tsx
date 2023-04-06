7a6eb0095b228cb3ea274bd0171fe0d8811a13f1
igor <igorek888rane@gmail.com>
2023-04-06 13:08:40 +0300

    Router test

--- src/tests/components/Router.test.tsx
+++ src/tests/components/Router.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import AdminPage from '../../pages/AdminPage'
 import { RenderWithRouter } from '../helpers/RenderWithRouter'
 import { Provider } from 'react-redux'
@@ -10,52 +10,64 @@ import CatalogPage from '../../pages/CatalogPage'
 import ProductPage from '../../pages/ProductPage'
 
 describe('Router', () => {
-	// test('Admin ', () => {
-	// 	render(
-	// 		<Provider store={store}>
-	// 			<RenderWithRouter>
-	// 				<AdminPage />
-	// 			</RenderWithRouter>
-	// 		</Provider>
-	// 	)
-	// 	const adminLink = screen.getByTestId('admin-link')
-	// 	userEvent.click(adminLink)
-	// 	expect(screen.getByTestId('admin-page')).toBeInTheDocument()
-	// })
-	// test('Cart ', () => {
-	// 	render(
-	// 		<Provider store={store}>
-	// 			<RenderWithRouter>
-	// 				<CartPage />
-	// 			</RenderWithRouter>
-	// 		</Provider>
-	// 	)
-	// 	const cartLink = screen.getByTestId('cart-link')
-	// 	userEvent.click(cartLink)
-	// 	expect(screen.getByTestId('cart-page')).toBeInTheDocument()
-	// })
-	// test('Catalog ', () => {
-	// 	render(
-	// 		<Provider store={store}>
-	// 			<RenderWithRouter initialRoute={'/cart'}>
-	// 				<CatalogPage />
-	// 			</RenderWithRouter>
-	// 		</Provider>
-	// 	)
-	// 	const catalogLink = screen.getByTestId('catalog-link')
-	// 	userEvent.click(catalogLink)
-	// 	expect(screen.getByTestId('catalog-page')).toBeInTheDocument()
-	// })
-	// test('Product ', () => {
-	// 	render(
-	// 		<Provider store={store}>
-	// 			<RenderWithRouter>
-	// 				<ProductPage />
-	// 			</RenderWithRouter>
-	// 		</Provider>
-	// 	)
-	// 	const productLink = screen.getAllByTestId('product-link')
-	// 	userEvent.click(productLink[0])
-	// 	expect(screen.getByTestId('product-page')).toBeInTheDocument()
-	// })
+	beforeAll(() => {
+		Element.prototype.scrollIntoView = jest.fn()
+	})
+	test('Admin ', async () => {
+		render(
+			<Provider store={store}>
+				<RenderWithRouter>
+					<AdminPage />
+				</RenderWithRouter>
+			</Provider>
+		)
+
+		await act(() => {
+			const adminLink = screen.getByTestId('admin-link')
+			userEvent.click(adminLink)
+			expect(screen.getByTestId('admin-page')).toBeInTheDocument()
+		})
+	})
+	test('Cart ', async () => {
+		render(
+			<Provider store={store}>
+				<RenderWithRouter>
+					<CartPage />
+				</RenderWithRouter>
+			</Provider>
+		)
+		await act(() => {
+			const cartLink = screen.getByTestId('cart-link')
+			userEvent.click(cartLink)
+			expect(screen.getByTestId('cart-page')).toBeInTheDocument()
+		})
+	})
+	test('Catalog ', async () => {
+		render(
+			<Provider store={store}>
+				<RenderWithRouter initialRoute={'/cart'}>
+					<CatalogPage />
+				</RenderWithRouter>
+			</Provider>
+		)
+		act(() => {
+			const catalogLink = screen.getByTestId('catalog-link')
+			userEvent.click(catalogLink)
+			expect(screen.getByTestId('catalog-page')).toBeInTheDocument()
+		})
+	})
+	test('Product ', async () => {
+		render(
+			<Provider store={store}>
+				<RenderWithRouter>
+					<ProductPage />
+				</RenderWithRouter>
+			</Provider>
+		)
+		await act(() => {
+			const productLink = screen.getAllByTestId('product-link')
+			userEvent.click(productLink[0])
+			expect(screen.getByTestId('product-page')).toBeInTheDocument()
+		})
+	})
 })
